f0a915df47a2b6e255639c64a2f7f0c63c817d45
reifnotreef <bridgerrhammond@gmail.com>
2019-12-11 13:09:24 -0600

    added funcs and updated size

--- arrayPractice.js
+++ arrayPractice.js
@@ -1,77 +1,80 @@
-const MEMORY = require('./memory');
+const MEMORY = require("./memory");
 let memory = new MEMORY();
 
 class customArray {
-    constructor(memAllo = 10) {
-        this.length = 0;
-        this.size = memAllo;
-        this.ptr = memory.allocate(memAllo);
-    }
-    
-    checkIndex(indx) {
-        if(indx < 0 || indx >= this.length) {
-            throw new Error('Index out of bounds.');
-        }
-    }
+  constructor(memAllo = 10) {
+    this.length = 0;
+    this.size = memAllo;
+    this.ptr = memory.allocate(memAllo);
+  }
 
-    checkMem() {
-        if(this.length + 1 > this.size) {
-            let newPtr = memory.allocate(this.size + 10);
-            memory.copy(newPtr, this.ptr, this.size);
-            
-            this.size += 10;
-            this.ptr = newPtr;
-        }
+  checkIndex(indx) {
+    if (indx < 0 || indx >= this.length) {
+      throw new Error("Index out of bounds.");
     }
+  }
+  prototype.toString = function() {
+    console.log("hello");
+  };
 
-    push(element) {
-        this.checkMem();
-        memory.set(this.ptr + this.length, element);
-        this.length++;
+  checkMem() {
+    if (this.length + 1 > this.size) {
+      let newPtr = memory.allocate(this.size * 2);
+      memory.copy(newPtr, this.ptr, this.size);
+
+      this.size * 2;
+      this.ptr = newPtr;
     }
+  }
 
-    pop() {
-        if(this.length > 0) {
-            let lastElement = memory.get(this.ptr + this.length - 1);
-            this.length--;
-            return lastElement;
-        } else throw new Error('Array must not be empty.');
-    }
+  push(element) {
+    this.checkMem();
+    memory.set(this.ptr + this.length, element);
+    this.length++;
+  }
 
-    get(indx) {
-        this.checkIndex(indx);
-        return memory.get(this.ptr + indx);
-    }
+  pop() {
+    if (this.length > 0) {
+      let lastElement = memory.get(this.ptr + this.length - 1);
+      this.length--;
+      return lastElement;
+    } else throw new Error("Array must not be empty.");
+  }
 
-    set(indx, value) {
-        this.checkIndex(indx);
-        memory.set(this.ptr + indx, value);
-    }
+  get(indx) {
+    this.checkIndex(indx);
+    return memory.get(this.ptr + indx);
+  }
 
-    insert(index, value) {
-        this.checkIndex(index);
-        this.checkMem();
+  set(indx, value) {
+    this.checkIndex(indx);
+    memory.set(this.ptr + indx, value);
+  }
 
-        memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
-        this.length++;
-        memory.set(this.ptr + index, value);
-    }
-    // | c|x|1 |2 |3 |4 | 5| 
-    // |c |1 |2 |3 |4 | 5| 5|
-    remove(index) {
-        this.checkIndex();
+  insert(index, value) {
+    this.checkIndex(index);
+    this.checkMem();
 
-        memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
-        this.length--;
-    }
+    memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+    this.length++;
+    memory.set(this.ptr + index, value);
+  }
+  // | c|x|1 |2 |3 |4 | 5|
+  // |c |1 |2 |3 |4 | 5| 5|
+  remove(index) {
+    this.checkIndex();
 
-    length() {
-        return this.length;
-    }
+    memory.copy(
+      this.ptr + index,
+      this.ptr + index + 1,
+      this.length - index - 1
+    );
+    this.length--;
+  }
+
+  length() {
+    return this.length;
+  }
 }
 
-let x = new customArray();
-
-x.push('something');
-
-console.log(x);
+module.exports = customArray;
---
+++ funcs.js
@@ -0,0 +1,26 @@
+const customArray = require("./arrayPractice");
+
+let arr = new customArray();
+
+console.log(arr);
+
+arr.push("test");
+arr.push(5);
+arr.push(15);
+arr.push(19);
+arr.push(45);
+arr.push(10);
+arr.push(5);
+arr.push(15);
+arr.push(19);
+arr.push(45);
+arr.push(10);
+arr.push(5);
+arr.push(15);
+arr.push(19);
+arr.push(45);
+arr.push("test");
+arr.pop();
+arr.pop();
+arr.pop();
+console.log(arr);
